59a9e848bd07fad323a9a73b1d30785b5c9aff8f
Wonjin <cwonjin1@gmail.com>
2020-12-13 12:01:08 -0500

    commit tree 4f09629e93d6a23a8cfdc5d55ce0bfd8dcdfcd55

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,4 +1,4 @@
-# birthday_kids = {
+#birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
@@ -6,6 +6,10 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+
 end
 
 
